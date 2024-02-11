88ee39e0d88153f0bb64f067b3815d1aa0de4832
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-02-11 14:20:25 +0200

    auto refreshing hook

--- hooks-customhooks/src/App.jsx
+++ hooks-customhooks/src/App.jsx
@@ -1,4 +1,8 @@
+// @ts-nocheck
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+// ? data fetching hooks
 
 // const App = () => {
 //   const [jpsData, setJpsData] = useState([]);
@@ -28,26 +32,81 @@ import React, { useEffect, useState } from "react";
 //   );
 // };
 
-// ? Using custom hook below as useTodos
+// ? Using custom hook below as useTodos => data fetching hooks
 
-const useTodos = () => {
+// const useTodos = () => {
+//   const [jpsData, setJpsData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/todos")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setJpsData(data);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   return { jpsData, loading };
+// };
+
+// const App = () => {
+//   const { jpsData, loading } = useTodos();
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   return (
+//     <>
+//       {jpsData.map((item) => {
+//         return <Data key={item.id} item={item} />;
+//       })}
+//     </>
+//   );
+// };
+
+// const Data = ({ item }) => {
+//   return (
+//     <>
+//       <p>{JSON.stringify(item)}</p>
+//     </>
+//   );
+// };
+
+// ? Auto Refreshing hook => polling the backend every after 10 seconds
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 100) + 1;
+};
+
+const useTodos = (n) => {
   const [jpsData, setJpsData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((data) => {
-        setJpsData(data);
-        setLoading(false);
-      });
-  }, []);
+    const value = setInterval(() => {
+      const randomNumber = getRandomNumber();
+      axios
+        .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
+        .then((res) => setJpsData([res.data]));
+      setLoading(false);
+    }, n * 1000);
+
+    const randomNumber = getRandomNumber();
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
+      .then((res) => setJpsData([res.data]));
+    setLoading(false);
+
+    return () => clearInterval(value); // (cleanup logic) this will clear the previous clock and reset the loading state after the update is complete and whenever the dependency is updated
+  }, [n]);
 
   return { jpsData, loading };
 };
 
 const App = () => {
-  const { jpsData, loading } = useTodos();
+  const { jpsData, loading } = useTodos(5);
 
   if (loading) {
     return <p>Loading...</p>;
