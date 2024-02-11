83652e75e52858ccf25fc8eeebd8133f0d493dc7
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-02-11 10:46:44 +0200

    custom hook- useTodos

--- hooks-customhooks/src/App.jsx
+++ hooks-customhooks/src/App.jsx
@@ -1,6 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-const App = () => {
+// const App = () => {
+//   const [jpsData, setJpsData] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/todos")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setJpsData(data);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <Data jpsData={jpsData} />
+//     </div>
+//   );
+// };
+
+// const Data = ({ jpsData }) => {
+//   return (
+//     <>
+//       {jpsData.map((item) => {
+//         return <p key={item.id}>{JSON.stringify(item)}</p>;
+//       })}
+//     </>
+//   );
+// };
+
+// ? Using custom hook below as useTodos
+
+const useTodos = () => {
   const [jpsData, setJpsData] = useState([]);
 
   useEffect(() => {
@@ -10,6 +40,12 @@ const App = () => {
         setJpsData(data);
       });
   }, []);
+
+  return jpsData;
+};
+
+const App = () => {
+  const jpsData = useTodos();
 
   return (
     <div>
@@ -27,4 +63,5 @@ const Data = ({ jpsData }) => {
     </>
   );
 };
+
 export default App;
