8a6dd55366aad98df2a085210c048042673faed3
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-02-13 12:13:20 +0200

    practicing react and hooks

--- hooks-customhooks/src/App.jsx
+++ hooks-customhooks/src/App.jsx
@@ -76,57 +76,83 @@ import axios from "axios";
 
 // ? Auto Refreshing hook => polling the backend every after 10 seconds
 
-const getRandomNumber = () => {
-  return Math.floor(Math.random() * 100) + 1;
-};
+// const getRandomNumber = () => {
+//   return Math.floor(Math.random() * 100) + 1;
+// };
 
-const useTodos = (n) => {
-  const [jpsData, setJpsData] = useState([]);
-  const [loading, setLoading] = useState(true);
+// const useTodos = (n) => {
+//   const [jpsData, setJpsData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const value = setInterval(() => {
+//       const randomNumber = getRandomNumber();
+//       axios
+//         .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
+//         .then((res) => setJpsData([res.data]));
+//       setLoading(false);
+//     }, n * 1000);
+
+//     const randomNumber = getRandomNumber();
+//     axios
+//       .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
+//       .then((res) => setJpsData([res.data]));
+//     setLoading(false);
+
+//     return () => clearInterval(value); // (cleanup logic) this will clear the previous clock and reset the loading state after the update is complete and whenever the dependency is updated----without this even though the new clock starts ( when changing the dependency which is n), the old clock does not stop, so you need to add the clean up logic to stop the old clock
+//   }, [n]);
+
+//   return { jpsData, loading };
+// };
+
+// const App = () => {
+//   const { jpsData, loading } = useTodos(5);
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   return (
+//     <>
+//       {jpsData.map((item) => {
+//         return <Data key={item.id} item={item} />;
+//       })}
+//     </>
+//   );
+// };
+
+// const Data = ({ item }) => {
+//   return (
+//     <>
+//       <p>{JSON.stringify(item)}</p>
+//     </>
+//   );
+// };
+
+function useISOnline() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
   useEffect(() => {
-    const value = setInterval(() => {
-      const randomNumber = getRandomNumber();
-      axios
-        .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
-        .then((res) => setJpsData([res.data]));
-      setLoading(false);
-    }, n * 1000);
+    window.addEventListener("online", () => setIsOnline(true));
+    window.addEventListener("offline", () => setIsOnline(false));
+  }, []);
 
-    const randomNumber = getRandomNumber();
-    axios
-      .get(`https://jsonplaceholder.typicode.com/todos/${randomNumber}`)
-      .then((res) => setJpsData([res.data]));
-    setLoading(false);
-
-    return () => clearInterval(value); // (cleanup logic) this will clear the previous clock and reset the loading state after the update is complete and whenever the dependency is updated
-  }, [n]);
-
-  return { jpsData, loading };
-};
+  return isOnline;
+}
 
 const App = () => {
-  const { jpsData, loading } = useTodos(5);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
+  useEffect(() => {
+    navigator.onLine ? setIsOnline(true) : setIsOnline(false);
+  }, []);
   return (
     <>
-      {jpsData.map((item) => {
-        return <Data key={item.id} item={item} />;
-      })}
-    </>
-  );
-};
-
-const Data = ({ item }) => {
-  return (
-    <>
-      <p>{JSON.stringify(item)}</p>
+      <div>{isOnline ? <p>Online</p> : <p>Offline</p>}</div>
     </>
   );
 };
 
 export default App;
+
+// ? Swr React Hooks for data fetching
+// Swr is a popular React library that creates a lot of these hooks which you can use directly
+// import useSWR from "swr"
--- react-intro/src/App.jsx
+++ react-intro/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 // const h1Style = {
@@ -5,33 +6,52 @@ import "./App.css";
 // };
 
 function App() {
+  // return (
+  //   <div>
+  //     <h1
+  //       style={{
+  //         color: "red",
+  //         fontFamily: "monospace",
+  //         textAlign: "center",
+  //       }}
+  //     >
+  //       MERN STACK
+  //     </h1>
+  //     <ul
+  //       style={{
+  //         display: "flex",
+  //         justifyContent: "flex-start",
+  //         alignItems: "center",
+  //         listStyle: "none",
+  //         fontSize: "2rem",
+  //         border: "1px solid black",
+  //         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+  //       }}
+  //     >
+  //       <li>Home</li>
+  //       <li>About</li>
+  //       <li>Contact</li>
+  //       <li>Blog</li>
+  //     </ul>
+  //   </div>
+  // );
+
+  // the count stored as const still being updated as in each render react creates a new count variable and stores somewhere in the memory. It preserves the count value throughout the render. React does not directly update the count or does not mutate its direct value.
+
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1
-        style={{
-          color: "red",
-          fontFamily: "monospace",
-          textAlign: "center",
+      <p>Count : {count}</p>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+          setCount((count) => count + 1);
+          console.log(count);
         }}
       >
-        MERN STACK
-      </h1>
-      <ul
-        style={{
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          listStyle: "none",
-          fontSize: "2rem",
-          border: "1px solid black",
-          boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-        }}
-      >
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Blog</li>
-      </ul>
+        Click me
+      </button>
     </div>
   );
 }
